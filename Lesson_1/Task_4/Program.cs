178d1db4ced98c9d66ea36a2ff08e6b7546476c5
Alexa <lisa_1511@mail.ru>
2023-12-26 01:55:16 +0300

    add lesson1:task4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 5;
+int b = 10;
+int c = 1;
+int d = 8;
+int e = 9;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
